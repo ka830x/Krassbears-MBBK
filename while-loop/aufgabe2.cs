bec11925646ce99c7b1699ce897fef1dd42559d4
ka830x <ka830@proton.me>
2025-01-15 18:21:13 +0100

    Ordner Struktur hinzugefügt

---
+++ while-loop/aufgabe2.cs
@@ -0,0 +1,9 @@
+﻿namespace while_loop;
+
+class aufgabe2
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ while-loop/aufgabe3.cs
@@ -0,0 +1,9 @@
+namespace while_loop;
+
+public class aufgabe3
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
