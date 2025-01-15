2dda085e30dd5268d07f46d87285d08512cd2ae0
ka830x <ka830@proton.me>
2025-01-15 18:44:53 +0100

    Files Restructure

---
+++ while-loop_aufgabe2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace while_loop_aufgabe2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string output = null;
+        int i = 0;
+        while (i < 10)
+        {
+            i++;
+            output += i.ToString() + " ";
+        }
+        Console.WriteLine(output);
+    }
+}
