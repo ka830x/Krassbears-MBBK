97507021bbba1f0397a08e7965c4e5e83bf22d82
ka830x <ka830@proton.me>
2025-01-15 18:43:47 +0100

    Files Restructure

--- while-loop/aufgabe2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace while_loop;
-
-class aufgabe2
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
--- while-loop/aufgabe3.cs
+++
@@ -1,9 +0,0 @@
-namespace while_loop;
-
-public class aufgabe3
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
