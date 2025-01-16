1ce07fc4b83e8ba91b10bd6b174843efc423e4c9
ka830x <kasparskoda@gmail.com>
2025-01-16 12:08:28 +0100

    do while aufgabe (zahlen raten)

---
+++ zahlen-raten/zahlen-raten.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Transactions;
+
+namespace zahlen_raten;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int target = new Random().Next(1, 12);
+        int guess = 0;
+
+        while (guess != target) 
+        { 
+                Console.WriteLine("enter Guess:");
+                guess = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        Console.WriteLine(target);
+        Console.WriteLine(guess);
+    }
+}
